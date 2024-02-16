4109697859b6ff6530e7b6f804b812ad8c89ec7e
Dranoel <dranoel512@proton.me>
2024-02-16 23:45:24 +0000

    Identifiers and basic data types

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ struct Token {
 enum TokenType {
     Identifier(String),
     String(String),
+    Int(i64),
+    Float(f64),
 
     Assign,
     Constant,
@@ -60,6 +62,31 @@ enum TokenType {
 
 struct Scanner {}
 
+macro_rules! token {
+    ($token_type:ident) => {
+        Ok(Token {
+            token_type: TokenType::$token_type,
+        })
+    };
+    ($token_type:ident($value:expr)) => {
+        Ok(Token {
+            token_type: TokenType::$token_type($value),
+        })
+    };
+}
+
+macro_rules! raw_error {
+    ($($t:tt)*) => {
+        io::Error::new(io::ErrorKind::InvalidData, format!($($t)*))
+    };
+}
+
+macro_rules! error {
+    ($($t:tt)*) => {
+        Err(raw_error!($($t)*))
+    };
+}
+
 impl Scanner {
     fn token(token_type: TokenType) -> io::Result<Token> {
         Ok(Token { token_type })
@@ -72,7 +99,7 @@ impl Scanner {
             let mut char = first_char;
 
             let mut is_end = false;
-            while char.is_whitespace() && !is_end {
+            while char.is_ascii_whitespace() && !is_end {
                 if let Some(next_char) = peekable.next() {
                     char = next_char;
                 } else {
@@ -81,7 +108,7 @@ impl Scanner {
             }
 
             if is_end {
-                Self::token(TokenType::EOF)
+                token!(EOF)
             } else {
                 match char {
                     '<' => {
@@ -89,16 +116,16 @@ impl Scanner {
                             match next {
                                 '-' => {
                                     peekable.next();
-                                    Self::token(TokenType::Assign)
+                                    token!(Assign)
                                 }
                                 '=' => {
                                     peekable.next();
-                                    Self::token(TokenType::LessThanOrEqualTo)
+                                    token!(LessThanOrEqualTo)
                                 }
-                                _ => Self::token(TokenType::LessThan),
+                                _ => token!(LessThan),
                             }
                         } else {
-                            Self::token(TokenType::LessThan)
+                            token!(LessThan)
                         }
                     }
 
@@ -107,43 +134,138 @@ impl Scanner {
                             match next {
                                 '=' => {
                                     peekable.next();
-                                    Self::token(TokenType::GreaterThanOrEqualTo)
+                                    token!(GreaterThanOrEqualTo)
                                 }
-                                _ => Self::token(TokenType::GreaterThan),
+                                _ => token!(GreaterThan),
                             }
                         } else {
-                            Self::token(TokenType::GreaterThan)
+                            token!(GreaterThan)
                         }
                     }
 
                     '!' => {
                         if let Some(next) = peekable.peek() {
                             match next {
-                                '=' => Self::token(TokenType::NotEqualTo),
-                                _ => Err(io::Error::new(
-                                    io::ErrorKind::InvalidData,
-                                    format!("Unexpected character: {}", next.to_string()),
-                                )),
+                                '=' => token!(NotEqualTo),
+                                _ => error!("Unexpected character: {}", next.to_string()),
                             }
                         } else {
-                            Err(io::Error::new(io::ErrorKind::InvalidData, "Unexpected EOF"))
+                            error!("Unexpected EOF")
                         }
                     }
 
-                    '+' => Self::token(TokenType::Add),
-                    '-' => Self::token(TokenType::Subtract),
-                    '*' => Self::token(TokenType::Multiply),
-                    '/' => Self::token(TokenType::Divide),
+                    '+' => token!(Add),
+                    '-' => token!(Subtract),
+                    '*' => token!(Multiply),
+                    '/' => token!(Divide),
 
-                    '=' => Self::token(TokenType::EqualTo),
+                    '=' => token!(EqualTo),
 
-                    '\n' => Self::token(TokenType::LineBreak),
+                    '\n' => token!(LineBreak),
 
-                    _ => Self::token(TokenType::String(char.to_string())),
+                    '\'' => {
+                        let mut string = String::new();
+
+                        let mut is_end = false;
+                        let mut escape = false;
+                        while let Some(char) = peekable.next() {
+                            match char {
+                                '\\' if !escape => {
+                                    escape = true;
+                                }
+                                '\'' if !escape => {
+                                    is_end = true;
+                                    break;
+                                }
+                                'n' if escape => {
+                                    string.push('\n');
+                                }
+                                _ => {
+                                    string.push(char);
+                                }
+                            }
+                        }
+
+                        if is_end {
+                            token!(String(string))
+                        } else {
+                            error!("Unexpected EOF")
+                        }
+                    }
+
+                    _ if char.is_ascii_digit() => {
+                        let mut string = String::new();
+                        string.push(char);
+
+                        let mut is_float = false;
+                        let mut error: Option<io::Error> = None;
+                        while let Some(char) = peekable.next() {
+                            if char.is_numeric() {
+                                string.push(char);
+                            } else if char == '.' {
+                                is_float = true;
+                                string.push(char);
+                            } else if char.is_ascii_whitespace() {
+                                break;
+                            } else {
+                                error = Some(io::Error::new(
+                                    io::ErrorKind::InvalidData,
+                                    format!("Unexpected character: {}", char),
+                                ));
+                                break;
+                            }
+                        }
+
+                        if let Some(error) = error {
+                            Err(error)
+                        } else {
+                            if is_float {
+                                let parse_result = string.parse::<f64>();
+                                if let Ok(result) = parse_result {
+                                    token!(Float(result))
+                                } else {
+                                    Err(io::Error::new(
+                                        io::ErrorKind::InvalidData,
+                                        "Failed to parse float",
+                                    ))
+                                }
+                            } else {
+                                let parse_result = string.parse::<i64>();
+                                if let Ok(result) = parse_result {
+                                    token!(Int(result))
+                                } else {
+                                    Err(io::Error::new(
+                                        io::ErrorKind::InvalidData,
+                                        "Failed to parse int",
+                                    ))
+                                }
+                            }
+                        }
+                    }
+
+                    _ if char.is_alphabetic() => {
+                        let mut string = String::new();
+                        string.push(char);
+
+                        while let Some(char) = peekable.next() {
+                            if char.is_alphanumeric() {
+                                string.push(char);
+                            } else {
+                                break;
+                            }
+                        }
+
+                        token!(Identifier(string))
+                    }
+
+                    _ => Err(io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        format!("Unexpected character: {}", char),
+                    )),
                 }
             }
         } else {
-            Self::token(TokenType::EOF)
+            token!(EOF)
         }
     }
 
